e85c445ba4f4cef1fd054d3356ce0cbe01a190cb
Konnor Lycka <konnor_lycka1@baylor.edu>
2024-09-05 15:37:55 -0500

    made changes

--- script1.py
+++ script1.py
@@ -3,14 +3,14 @@
 # Johnny, the total of your purchase is $1,688.70
 
 def calcTotal(amt,tax,name):
-    mytotal = amt * (1+tax)
-    print(f"{myname}, the total of your purchase is ${total:,.2f}")
+    mytotal = amt * (1+ thetax)
+    print(f"{myname}, the total of your purchase is ${mytotal:,.2f}")
 
 myname = 'Johnny'
-price = 1,560
-thetax = 8.25%
+price = 1560
+thetax = .0825
 
-calcTotal(thetax,price,name)
+calcTotal(price,thetax,myname)
 
 
 
@@ -21,7 +21,10 @@ calcTotal(thetax,price,name)
 list1 = [1,'two',3.2,'four',5]
 
 for i in list1:
-    print(i)
+    if isinstance(i,int):
+        print(i)
+#if type(i) == int
+#type tells what type youre working with 
 
 
 
